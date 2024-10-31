f7e6f22eb4ee6bc0b084e9dddfb57fb04b72a6ac
Youssef-001 <youssefsamir432@gmail.com>
2024-10-31 14:17:51 +0300

    Add controller and handlers

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.get("/*", (req, res) =>
   res.sendFile(path.join(__dirname, "public", "pages/404.html"))
 );
 
+app.use((err, req, res, next) => {
+  console.error(err);
+  // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
+  res.status(err.statusCode || 500).send(err.message);
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
