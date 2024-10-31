53e7d86f3057bef776cda3a17dd935205ab529f7
Youssef-001 <youssefsamir432@gmail.com>
2024-10-31 14:34:11 +0300

    Fix paths

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
-const CustomNotFoundError = require("../errors/CustomNotFoundError");
-// The function will automatically catch any errors thrown and call the next function
+const db = require("../db");
+const CustomNotFoundError = require("../errors/CustomNotFoundError"); // The function will automatically catch any errors thrown and call the next function
 const getAuthorById = asyncHandler(async (req, res) => {
   const { authorId } = req.params;
 
@@ -9,6 +9,6 @@ const getAuthorById = asyncHandler(async (req, res) => {
   if (!author) {
     throw new CustomNotFoundError("Author not found");
   }
-
   res.send(`Author Name: ${author.name}`);
 });
+module.exports = { getAuthorById };
